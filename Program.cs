ab4928d82cab1d541221e5382e3f53f7bce1089e
dyahsari <dyahsari@students.amikom.ac.id>
2023-03-31 09:29:40 +0700

    taxi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taksi taxi = new Taksi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+
+            Console.WriteLine();
+
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
